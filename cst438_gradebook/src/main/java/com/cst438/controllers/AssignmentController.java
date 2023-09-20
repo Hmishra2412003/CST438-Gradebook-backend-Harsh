f0b4cd904a18d7f4b8fb7d8a8b31694a51b1719f
Harsh <hmishra@csumb.edu>
2023-09-20 01:07:32 -0700

    test cases

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -22,15 +22,15 @@ import com.cst438.domain.Course;
 import com.cst438.domain.CourseRepository;
 
 @RestController
-@CrossOrigin 
+@CrossOrigin
 public class AssignmentController {
-	
+
 	@Autowired
 	AssignmentRepository assignmentRepository;
-	
+
 	@Autowired
 	CourseRepository courseRepository;
-	
+
 	@GetMapping("/assignment")
 	public AssignmentDTO[] getAllAssignmentsForInstructor() {
 		// get all assignments for this instructor
@@ -40,15 +40,87 @@ public class AssignmentController {
 		for (int i=0; i<assignments.size(); i++) {
 			Assignment as = assignments.get(i);
 			AssignmentDTO dto = new AssignmentDTO(
-					as.getId(), 
-					as.getName(), 
-					as.getDueDate().toString(), 
-					as.getCourse().getTitle(), 
+					as.getId(),
+					as.getName(),
+					as.getDueDate().toString(),
+					as.getCourse().getTitle(),
 					as.getCourse().getCourse_id());
 			result[i]=dto;
 		}
 		return result;
 	}
-	
-	// TODO create CRUD methods for Assignment
+	@PostMapping("/assignment")
+	public AssignmentDTO addAssignment(@RequestBody AssignmentDTO assignmentDTO) {
+		// Create a new Assignment object
+		Assignment newAssignment = new Assignment();
+
+		// Set the name property of the Assignment object using assignmentDTO
+		newAssignment.setName(assignmentDTO.assignmentName());
+
+		// Set other assignment properties like due date, course, etc.
+
+		// Save the new assignment to the database
+		Assignment savedAssignment = assignmentRepository.save(newAssignment);
+
+		// Convert the saved assignment to DTO and return it
+		AssignmentDTO savedDTO = new AssignmentDTO(
+				savedAssignment.getId(),
+				savedAssignment.getName(), // You can use savedAssignment.getName() here
+				savedAssignment.getDueDate().toString(),
+				savedAssignment.getCourse().getTitle(),
+				savedAssignment.getCourse().getCourse_id()
+		);
+		return savedDTO;
+	}
+
+
+	@GetMapping("/assignment/{id}")
+	public AssignmentDTO getAssignmentById(@PathVariable int id) {
+		// Find the assignment by its ID
+		Assignment assignment = assignmentRepository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Assignment not found"));
+
+		// Convert the assignment to DTO and return it
+		AssignmentDTO assignmentDTO = new AssignmentDTO(
+				assignment.getId(),
+				assignment.getName(),
+				assignment.getDueDate().toString(),
+				assignment.getCourse().getTitle(),
+				assignment.getCourse().getCourse_id()
+		);
+		return assignmentDTO;
+	}
+	@PutMapping("/assignment/{id}")
+	public AssignmentDTO updateAssignment(@PathVariable int id, @RequestBody AssignmentDTO updatedAssignmentDTO) {
+		// Find the assignment by its ID
+		Assignment assignment = assignmentRepository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Assignment not found"));
+
+		// Update the assignment properties from the DTO
+		assignment.setName(updatedAssignmentDTO.assignmentName());
+		// Update other assignment properties as needed
+
+		// Save the updated assignment to the database
+		Assignment updatedAssignment = assignmentRepository.save(assignment);
+
+		// Convert the updated assignment to DTO and return it
+		AssignmentDTO updatedDTO = new AssignmentDTO(
+				updatedAssignment.getId(),
+				updatedAssignment.getName(),
+				updatedAssignment.getDueDate().toString(),
+				updatedAssignment.getCourse().getTitle(),
+				updatedAssignment.getCourse().getCourse_id()
+		);
+		return updatedDTO;
+	}
+	@DeleteMapping("/assignment/{id}")
+	public void deleteAssignment(@PathVariable int id) {
+		// Check if the assignment exists
+		Assignment assignment = assignmentRepository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Assignment not found"));
+
+		// Delete the assignment
+		assignmentRepository.delete(assignment);
+	}
+
 }
--- cst438_gradebook/src/main/java/com/cst438/domain/Assignment.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/Assignment.java
@@ -13,51 +13,70 @@ import javax.persistence.OneToMany;
 
 @Entity
 public class Assignment {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
 	@ManyToOne
-	@JoinColumn(name="course_id")
+	@JoinColumn(name = "course_id")
 	private Course course;
-	
-	@OneToMany(mappedBy="assignment")
+
+	@OneToMany(mappedBy = "assignment")
 	private List<AssignmentGrade> assignmentGrades;
-	
+
 	private String name;
 	private Date dueDate;
 
-	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Date getDueDate() {
 		return dueDate;
 	}
+
 	public void setDueDate(Date dueDate) {
 		this.dueDate = dueDate;
 	}
-	
+
 	public Course getCourse() {
 		return course;
 	}
+
 	public void setCourse(Course course) {
 		this.course = course;
 	}
+
+	public List<AssignmentGrade> getAssignmentGrades() {
+		return assignmentGrades;
+	}
+
+	public void setAssignmentGrades(List<AssignmentGrade> assignmentGrades) {
+		this.assignmentGrades = assignmentGrades;
+	}
+
 	@Override
 	public String toString() {
-		return "Assignment [id=" + id + ", course_id=" + course.getCourse_id() + ", name=" + name + ", dueDate=" + dueDate
-				+ "]";
+		return "Assignment [id=" + id + ", course_id=" + course.getCourse_id() + ", name=" + name + ", dueDate="
+				+ dueDate + "]";
 	}
-	
+	// Constructor to ensure dueDate is not null when creating an Assignment
+	public Assignment() {
+		this.dueDate = new Date(System.currentTimeMillis()); // Initialize with the current date
+
+		this.course = new Course(); // Initialize with a new Course instance
+	}
 }
